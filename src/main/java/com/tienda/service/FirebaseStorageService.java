63cc5a8f97ff64cdfb3ee16650e37358befcece6
RachellMonge <rachellmonge33@gmail.com>
2024-02-22 20:45:59 -0600

    semana 6

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,24 @@
+
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-v1-l-8464e.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-v1-l-8464e-firebase-adminsdk-9bbkt-571edf8fa5";
+}
+
